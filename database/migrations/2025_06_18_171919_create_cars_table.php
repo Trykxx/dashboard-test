e2a7c356ed267690cd3c9e6103c766103d46cc81
Trykxx <amitnicolas@gmail.com>
2025-06-18 20:47:32 +0200

    create car model

---
+++ database/migrations/2025_06_18_171919_create_cars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('marque');
+            $table->string('modele');
+            $table->integer('annee');
+            $table->string('couleur');
+            $table->integer('nb_portes');
+            $table->string('carburant');
+            $table->string('transmission');
+            $table->decimal('prix', 10, 2);
+            $table->integer('kilometrage');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
